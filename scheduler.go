d0c6e0a435a607030d4ec04b39e3615878f39fc4
Eric Fritz <eric@eric-fritz.com>
2016-06-01 21:49:44 -0500

    Refactor code.

--- scheduler.go
+++ scheduler.go
@@ -3,61 +3,42 @@ package recurrent
 import "time"
 
 type Scheduler struct {
-	quit   chan bool
-	signal chan bool
+	target   func()
+	interval time.Duration
+	quit     chan bool
+	signal   chan bool
 }
 
-func NewScheduler(interval time.Duration, f func()) *Scheduler {
-	sched := &Scheduler{
-		quit:   make(chan bool),
-		signal: make(chan bool, 1),
-	}
+func NewScheduler(interval time.Duration, target func()) *Scheduler {
+	return newScheduler(interval, target, func(sched *Scheduler) {
+		c := make(chan bool)
+		q := make(chan bool)
 
-	go func() {
-		for {
-			select {
-			case <-time.After(interval):
-				sched.Signal()
+		go func() {
+			for {
+				select {
+				case <-q:
+					break
 
-			case <-sched.signal:
-				f()
-
-			case <-sched.quit:
-				break
+				case c <- true:
+				}
 			}
-		}
-	}()
 
-	return sched
+			close(c)
+		}()
+
+		sched.run(c)
+		q <- true
+	})
 }
 
-func NewThrottledScheduler(interval time.Duration, minInterval time.Duration, f func()) *Scheduler {
-	sched := &Scheduler{
-		quit:   make(chan bool),
-		signal: make(chan bool, 1),
-	}
-
-	go func() {
+func NewThrottledScheduler(interval time.Duration, minInterval time.Duration, target func()) *Scheduler {
+	return newScheduler(interval, target, func(sched *Scheduler) {
 		tick := time.NewTicker(minInterval)
 		defer tick.Stop()
 
-		t := throttle(tick.C, sched.signal)
-
-		for {
-			select {
-			case <-time.After(interval):
-				sched.Signal()
-
-			case <-t:
-				f()
-
-			case <-sched.quit:
-				break
-			}
-		}
-	}()
-
-	return sched
+		sched.run(convert(tick.C))
+	})
 }
 
 func (sched *Scheduler) Stop() {
@@ -72,13 +53,58 @@ func (sched *Scheduler) Signal() {
 	}
 }
 
-func throttle(a <-chan time.Time, b chan bool) chan bool {
+func newScheduler(interval time.Duration, target func(), init func(*Scheduler)) *Scheduler {
+	sched := &Scheduler{
+		target:   target,
+		interval: interval,
+		quit:     make(chan bool),
+		signal:   make(chan bool, 1),
+	}
+
+	go init(sched)
+	return sched
+}
+
+func (sched *Scheduler) run(c chan bool) {
+	t := throttle(c, sched.signal)
+
+	for {
+		select {
+		case <-time.After(sched.interval):
+			sched.Signal()
+
+		case <-t:
+			sched.target()
+
+		case <-sched.quit:
+			break
+		}
+	}
+}
+
+func convert(a <-chan time.Time) chan bool {
+	c := make(chan bool)
+
+	go func() {
+		for _ = range a {
+			c <- true
+		}
+
+		close(c)
+	}()
+
+	return c
+}
+
+func throttle(a chan bool, b chan bool) chan bool {
 	c := make(chan bool)
 
 	go func() {
 		for _ = range a {
 			c <- <-b
 		}
+
+		close(c)
 	}()
 
 	return c
